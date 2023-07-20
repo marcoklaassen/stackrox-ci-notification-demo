e6c5e2c3dc61c384ef47a87cd7006d2462139213
Marco Klaassen <mklaasse@redhat.com>
2023-07-20 13:00:46 +0200

    fix add slack integration script

--- acs-install/06_add-slack-notification.sh
+++ acs-install/06_add-slack-notification.sh
@@ -1,5 +1,5 @@
 export ROX_CENTRAL_PASSWORD=$(oc get secret central-htpasswd -n rhacs --template={{.data.password}} | base64 -d)
-export ROX_CENTRAL_HOST=$(oc get route central -n rhacs --template={{.spec.host}})
+export ROX_CENTRAL_HOST=$(oc get route central-grpc -n rhacs --template={{.spec.host}})
 curl 'https://'$ROX_CENTRAL_HOST'/v1/notifiers' \
     -H 'content-type: application/json' \
     -u 'admin:'$ROX_CENTRAL_PASSWORD \
